4e3fbc1bd7307f4298b48eacdc7317fc325e42bc
serioja8bit <sergiuleva@gmail.com>
2023-03-23 15:07:05 +0200

    react schimbari

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1> Sergiu Leva </h1>
+        <p> CR-201 </p>
+    </div>
+  );
+}
+
+export default App;
