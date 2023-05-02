7c62fb9726dd730f3d8d1712b7ea49c0a60d4e5a
serioja8bit <sergiuleva@gmail.com>
2023-05-02 12:30:55 +0300

    lab4 Modificar

--- src/MyInterface.js
+++ src/MyInterface.js
@@ -1,10 +1,15 @@
 
 
-export interface Pokemon {
+export interface Animal {
     type: string;
     image: string;
     name: string;
     weight: number;
     height: number;
 
+}
+
+export interface Pokemon extends Animal{
+    description: string;
+    data: string;
 }
